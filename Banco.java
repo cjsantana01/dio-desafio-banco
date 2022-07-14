3ae83eed94438a5323dc6fb05ad12455b21d0023
Clayton Joaquim <87860753+cjsantana01@users.noreply.github.com>
2022-07-14 15:47:48 -0300

    Add files via upload

---
+++ Banco.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Banco {
+
+    private String nome;
+    private List<Conta> contas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setContas() {
+        this.nome = nome;
+    }
+
+    public String getContas() {
+        return nome;
+    }
+
+    public void setNome() {
+        this.nome = nome;
+    }
+}
---
+++ Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome() {
+        this.nome = nome;
+    }
+}
